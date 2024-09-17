571a10b921fee5f1969a95be04c7e56fd96f2d86
Austin Rosenberg <austin.rosenberg@databricks.com>
2024-09-17 09:42:06 -0400

    fix unit test

--- default_python/src/default_python/main.py
+++ default_python/src/default_python/main.py
@@ -9,28 +9,28 @@ def get_taxis(spark: SparkSession) -> DataFrame:
 # check that you have configured Databricks Connect correctly.
 # See https://docs.databricks.com/dev-tools/databricks-connect.html.
 def get_spark() -> SparkSession:
-  try:
-    # from databricks.connect import DatabricksSession
-    # spark = DatabricksSession.builder.remote(
-    #   host       = f"https://e2-demo-field-eng.cloud.databricks.com/",
-    #   token      = os.environ['DATABRICKS_PAT'],
-    #   cluster_id = "0730-180525-bdpqfev2"
-    # ).getOrCreate()
-    from databricks.connect import DatabricksSession
-    from databricks.sdk.core import Config
+# try:
+  # from databricks.connect import DatabricksSession
+  # spark = DatabricksSession.builder.remote(
+  #   host       = f"https://e2-demo-field-eng.cloud.databricks.com/",
+  #   token      = os.environ['DATABRICKS_PAT'],
+  #   cluster_id = "0730-180525-bdpqfev2"
+  # ).getOrCreate()
+  from databricks.connect import DatabricksSession
+  from databricks.sdk.core import Config
 
-    pat = os.environ['DATABRICKS_PAT']
-    print("this is the pat " + pat)
-    print("this is a test " + os.environ['TEST'])
+  pat = os.environ['DATABRICKS_PAT']
+  print("this is the pat " + pat)
+  print("this is a test " + os.environ['TEST'])
 
-    config = Config(
-      host       = f"https://e2-demo-field-eng.cloud.databricks.com/",
-      token      = pat,
-      cluster_id = "0730-180525-bdpqfev2"
-    )
-    spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
-  except ImportError:
-    return SparkSession.builder.getOrCreate()
+  config = Config(
+    host       = f"https://e2-demo-field-eng.cloud.databricks.com/",
+    token      = pat,
+    cluster_id = "c34b133fe1869017"
+  )
+  spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
+# except ImportError:
+#   return SparkSession.builder.getOrCreate()
 
 def main():
   get_taxis(get_spark()).show(5)
